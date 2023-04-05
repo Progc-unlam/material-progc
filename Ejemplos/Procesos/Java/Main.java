01589a50377e6abce2e792844424ff18f1bef22b
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-04-05 14:37:48 -0300

    Update Main.java

--- Ejemplos/Procesos/Java/Main.java
+++ Ejemplos/Procesos/Java/Main.java
@@ -3,22 +3,17 @@ import java.io.IOException;
 public class Main
 {
     public static void main(String[] args) throws IOException,  InterruptedException
-	{
-        try
-		{
-            ProcessBuilder pb = new ProcessBuilder("java", "HolaMundo.java");
-            
-			pb.redirectErrorStream(true);
-            pb.inheritIO();
-            
-			Process proceso = pb.start();
-            
-			int error = proceso.waitFor();
+    {
+        ProcessBuilder pb = new ProcessBuilder("java", "HolaMundo.java");
+        pb.redirectErrorStream(true);
+        pb.inheritIO();
+        
+        Process proceso = pb.start();
+        int error = proceso.waitFor();
 			
-			if( error != 0 )
-			{
-				System.out.println("Error al ejecutar el programa");
-			}
+        if( error != 0 )
+        {
+            System.out.println("Error al ejecutar el programa");
         }
     }
 }
