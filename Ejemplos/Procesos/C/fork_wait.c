1a782de47b9a82377b7cf9d273e5e0452bf61dbb
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 15:19:58 -0300

    Update fork_wait.c

--- Ejemplos/Procesos/C/fork_wait.c
+++ Ejemplos/Procesos/C/fork_wait.c
@@ -5,28 +5,26 @@
 
 int main(int argc, char *argv[])
 {
-	pid_t pid;
-	
-  	pid = fork();
-	
-	if( pid < 0 )
-	{
-		printf("Error al crear el nuevo proceso\n");
-  		return EXIT_FAILURE;
-	}
-	
-	if( pid )
-	{
-		printf("Soy el proceso padre. PID: %d\n",getpid());
-		wait(NULL);
-	}
-	else
-	{
-		printf("Soy el proceso hijo. PID: %d\n",getpid());
-		printf("Mi padre es el proceso con PID: %d\n",getppid());
-		return EXIT_SUCCESS;	
-	}
-	
-  	return EXIT_SUCCESS;
-}
+    pid_t pid;
+    pid = fork();
 
+    if( pid < 0 )
+    {
+        printf("Error al crear el nuevo proceso\n");
+        return EXIT_FAILURE;
+    }
+    
+    if( pid )
+    {
+        printf("Soy el proceso padre. PID: %d\n",getpid());
+        wait(NULL);
+    }
+    else
+    {
+        printf("Soy el proceso hijo. PID: %d\n",getpid());
+        printf("Mi padre es el proceso con PID: %d\n",getppid());
+        return EXIT_SUCCESS;	
+    }
+
+    return EXIT_SUCCESS;
+}
