a4be993852aca206a36a857045f276d62d4cb208
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 17:43:15 -0300

    Update Main.java

--- Ejemplos/Procesos/Java/Main.java
+++ Ejemplos/Procesos/Java/Main.java
@@ -1,19 +1,27 @@
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
-public class Main
+public class EjemploProcess
 {
-    public static void main(String[] args) throws IOException,  InterruptedException
+    public static long getProcessId()
     {
+        return ManagementFactory.getRuntimeMXBean().getPid();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
+        System.out.println("PID: " + getProcessId());
+
         ProcessBuilder pb = new ProcessBuilder("java", "HolaMundo.java");
         pb.redirectErrorStream(true);
         pb.inheritIO();
         
         Process proceso = pb.start();
         int error = proceso.waitFor();
-			
+		
         if( error != 0 )
         {
-            System.out.println("Error al ejecutar el programa");
+            System.out.println("HolaMundo.java finalizó con error");
         }
     }
 }
