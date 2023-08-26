41f30b5b67cde7c3bc8b4ba591697d51496dfd7c
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 15:35:53 -0300

    Update llamado_desde_exec.c

--- Ejemplos/Procesos/C/llamado_desde_exec.c
+++ Ejemplos/Procesos/C/llamado_desde_exec.c
@@ -4,11 +4,8 @@
 
 int main( int argc, char *argv[] )
 {
-	printf("Programa B ejecutado con execv. PID: %d\n", getpid() );
-
-	printf("Nombre: %s\n", argv[0]);
- 	printf("Arg 1: %s\n", argv[1]);
- 	
-	return EXIT_SUCCESS;	
+    printf("Programa B ejecutado con execv. PID: %d\n", getpid() );
+    printf("Nombre: %s\n", argv[0]);
+    printf("Arg 1: %s\n", argv[1]);
+    return EXIT_SUCCESS;	
 }
-
