589da85ebe88bf07ad7512a36816754ea8dcd956
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 15:34:45 -0300

    Update exec_llamador.c

--- Ejemplos/Procesos/C/exec_llamador.c
+++ Ejemplos/Procesos/C/exec_llamador.c
@@ -2,45 +2,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include  <errno.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-	char *arg[] = { "B.bin", "Hola mundo", NULL };
-	pid_t pid;
-	int estado;
-	
-  	pid = fork();
-	
-	if( pid < 0 )
-	{
-		printf("Error al crear el nuevo proceso\n");
-  		return EXIT_FAILURE;
-	}
-	
-	if( pid )
-	{
-		printf("Soy el proceso padre. PID: %d\n",getpid());
-		
-		wait( &estado );
-		
-		if( estado == 0 )
-		{
-			printf("Hijo finalizado con éxito\n");
-		}
-		else
-		{
-			printf("Hijo finalizado con error\n");
-		}
-	}
-	else
-	{
-		execv("B.bin", arg );
-		printf("Error: %d\n",errno);	
-		perror("execv");
-		exit(EXIT_FAILURE);		
-	}
-	
-  	return EXIT_SUCCESS;
-}
+    char *arg[] = { "B.bin", "Hola mundo", NULL };
+    pid_t pid;
+    int estado;
 
+    pid = fork();
+    if( pid < 0 )
+    {
+        printf("Error al crear el nuevo proceso\n");
+        return EXIT_FAILURE;
+    }
+
+    if( pid )
+    {
+        printf("Soy el proceso padre. PID: %d\n",getpid());
+        wait( &estado );
+
+        if( estado == 0 )
+        {
+            printf("Hijo finalizado con éxito\n");
+        }
+        else
+        {
+            printf("Hijo finalizado con error\n");
+        }
+    }
+    else
+    {
+        execv("B.bin", arg );
+        printf("Error: %d\n",errno);	
+        perror("execv");
+        exit(EXIT_FAILURE);		
+    }
+
+    return EXIT_SUCCESS;
+}
