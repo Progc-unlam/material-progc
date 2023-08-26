4be31303b4ba49920590d4bb11e988c43c338a39
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 14:18:52 -0300

    Update fork_wait.py

--- Ejemplos/Procesos/Python/fork_wait.py
+++ Ejemplos/Procesos/Python/fork_wait.py
@@ -1,18 +1,15 @@
 import os
-	
-def parent():
-    pid = os.fork()
+import sys
 
-    if pid < 0:
-        print("Error al crear el nuevo proceso")
-	os._exit(1)
-		
-    if pid:
-        print("Soy el proceso padre. PID: ", os.getpid() )
-        print( os.wait() )
-    else:
-        print("Soy el proceso hijo. PID: ",  os.getpid())
-        print("Mi padre es el proceso con PID: ",  os.getppid())
-        os._exit(0)
+pid = os.fork()
 
-parent()
+if pid < 0:
+    sys.exit("Error al crear el nuevo proceso")
+
+if pid:
+    print("Soy el proceso padre. PID: ", os.getpid() )
+    print( os.wait() )
+else:
+    print("Soy el proceso hijo. PID: ",  os.getpid())
+    print("Mi padre es el proceso con PID: ",  os.getppid())
+    os._exit( os.EX_OK )
