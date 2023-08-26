cf9e849f59fdbe5d0a276cccb04d10e31c2b3627
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-08-26 17:31:52 -0300

    Update HolaMundo.java

--- Ejemplos/Procesos/Java/HolaMundo.java
+++ Ejemplos/Procesos/Java/HolaMundo.java
@@ -1,8 +1,11 @@
+import java.lang.management.ManagementFactory;
+
 class HolaMundo 
 {
-	public static void main(String[] args)
-	{
-    	System.out.println("Hola Mundo!");
-  	}
+    public static void main(String[] args)
+    {
+        long pid = ManagementFactory.getRuntimeMXBean().getPid();
+        System.out.println("PID: " + pid);
+        System.out.println("Hola Mundo!");
+	}
 }
-
