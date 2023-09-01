efec64876c9551a0af1c9be8c2b32e40b267e054
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-01 18:11:54 -0300

    Update Ejemplo_hilos_6.cpp

--- Ejemplos/Hilos/C++/Ejemplo_hilos_6.cpp
+++ Ejemplos/Hilos/C++/Ejemplo_hilos_6.cpp
@@ -1,1 +1,20 @@
+#include <thread>
+#include <iostream>
 
+void Sleep()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout<<"Finalizando hilo"<<std::endl;
+};
+
+int main( int argc, char *argv[] )
+{
+    std::thread hilo_a(Sleep);
+    //std::jthread hilo_a(Sleep);
+
+    //std::this_thread::sleep_for(std::chrono::seconds(4));
+
+    std::cout<<"Hilo principal finaliza."<<std::endl;
+
+    return EXIT_SUCCESS;
+}
