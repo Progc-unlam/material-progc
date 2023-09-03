77310df3a7f5d6c860b4397225de599fd4a9e240
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-03 13:46:51 -0300

    Update and rename Hilos_Java_ExtendThread.java to ExtendThread.java

---
+++ Ejemplos/Hilos/Java/ExtendThread.java
@@ -0,0 +1,19 @@
+public class ExtendThread
+{
+    public static class MyThread extends Thread
+    {
+        public void run()
+        {
+            System.out.println( "TID:    " + this.getId() );
+            System.out.println(	"Nombre: " + this.getName() );
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException 
+    {
+        MyThread testThread = new MyThread();
+
+        testThread.start();		
+        testThread.join();        
+    }
+}
