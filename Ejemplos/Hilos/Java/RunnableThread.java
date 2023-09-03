fed271e26287c8b435b2613e0b7c3450bb82bddb
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-03 14:25:33 -0300

    Update and rename HiloRunnable.java to RunnableThread.java

---
+++ Ejemplos/Hilos/Java/RunnableThread.java
@@ -0,0 +1,20 @@
+public class RunnableThread
+{
+    public static class MyThread implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            System.out.println( "TID:    " + Thread.currentThread().getId() );
+            System.out.println(	"Nombre: " + Thread.currentThread().getName() );
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException 
+    {
+        Thread testThread = new Thread(new MyThread(), "A");
+
+        testThread.start();
+        testThread.join();
+    }
+}
