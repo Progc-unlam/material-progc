fa5e496b34f5975202a3ce786134d709646ed1d0
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 12:39:34 -0300

    Update mcB.c

--- Ejemplos/Comunicacion/C/mcB.c
+++ Ejemplos/Comunicacion/C/mcB.c
@@ -2,29 +2,21 @@
 #include <stdio.h>
 #include <sys/ipc.h>  
 #include <sys/shm.h> 	
-#include <fcntl.h>	
-#include <errno.h>	
 
+#define SEG_ID 1234 
+#define TAM 5
 
-#define SEG_ID	1234 
-#define TAM 	5
+int main()
+{
+    int shmid = shmget(SEG_ID, sizeof(int)*TAM, 0600);	
 
-int main(){	
-	int shmid = shmget(SEG_ID, sizeof(int)*TAM, 00600);	
-	
-	int *mc = (int*)shmat( shmid, NULL, 0);
-	
-	for( int i=0; i< TAM; i++ )
-	{ 
-		printf("Proceso B leyó %d\n", mc[ i ] );
-	}
+    int *mc = (int*)shmat(shmid, NULL, 0);
 
-	struct shmid_ds pepe;	
-	shmdt( &mc );	
-	return EXIT_SUCCESS;
+    for(int i = 0; i < TAM; i++)
+    { 
+        printf("Proceso B leyó %d\n", mc[i]);
+    }
+
+    shmdt(&mc);	
+    return EXIT_SUCCESS;
 }
-
-
-
-
-
