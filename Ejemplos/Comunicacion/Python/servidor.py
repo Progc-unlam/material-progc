5b4b70b1a7af0c81a3f2adc8146000ffe487b56a
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 15:17:30 -0300

    Update servidor.py

--- Ejemplos/Comunicacion/Python/servidor.py
+++ Ejemplos/Comunicacion/Python/servidor.py
@@ -1,32 +1,33 @@
 import socket
 
 MAX_PEND = 10
-PUERTO   = 5000
+PUERTO = 5000
 
 def servidor():
-	#Creación del socket
-	ss = socket.socket()
+    #Creación del socket
+    ss = socket.socket()
 
-	#Vincular puerto y dirección IP con el socket
-	ss.bind( ( "127.0.0.1", PUERTO ) )
+    #Vincular puerto y dirección IP con el socket
+    ss.bind(("127.0.0.1", PUERTO))
 
-	#Ponemos en modo escucha y establecemos la
-	#cantidad de peticiones que puede encolar
-	ss.listen( MAX_PEND )
+    #Ponemos en modo escucha y establecemos la
+    #cantidad de peticiones que puede encolar
+    ss.listen(MAX_PEND)
 
-	#Espera por una petición de conexión
-	( cs, dir ) = ss.accept()
-	
-	while True:
-		recibi = cs.recv( 1024 ).decode( "ascii" )
-		if recibi == "fin":
-			break
-		print( recibi )
-		
-	print( "finalizado" )
+    #Espera por una petición de conexión
+    (cs, dir) = ss.accept()
 
-	#Cierre de los sockets
-	ss.close()
-	cs.close()
+    while True:
+        recibi = cs.recv(1024).decode("ascii")
+        if recibi == "fin":
+            break
+        print(recibi)
 
-servidor()	
+    print("finalizado")
+
+    #Cierre de los sockets
+    ss.close()
+    cs.close()
+
+if __name__ == '__main__':
+    servidor()
