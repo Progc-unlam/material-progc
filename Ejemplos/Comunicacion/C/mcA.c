c47e9f04876d295d1846e802d551d5db6b78f4c5
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 12:34:05 -0300

    Update mcA.c

--- Ejemplos/Comunicacion/C/mcA.c
+++ Ejemplos/Comunicacion/C/mcA.c
@@ -1,34 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/ipc.h> 	//Biblioteca para los flags IPC_ 
-#include <sys/shm.h> 	//Para memoria compartida SYSTEM-V
+#include <sys/shm.h> 	//Para memoria compartida (MC) SYSTEM-V
 #include <unistd.h>
 
-#define SEG_ID 1234	//La clave también puede ser creada con ftok()
+//La clave (SEG_ID) también puede ser creada con ftok()
+#define SEG_ID 1234	
 #define TAM 5
 
-int main(){
-	//Creación de memoria compartida
-	int shmid = shmget(	SEG_ID, sizeof(int)*TAM, IPC_CREAT | IPC_EXCL | 0600);	
+int main()
+{
+    //Creación del área de MC
+    int shmid = shmget(	SEG_ID, sizeof(int)*TAM, IPC_CREAT | IPC_EXCL | 0600);	
 
-	//Vincular la memoria compartida a una variable local
-	int *mc = (int*)shmat(shmid,NULL,0);
-	
-	for( int i=0; i< TAM; i++ )
-	{ 
-		mc[ i ] = i+1;
-		printf("Proceso A escribió %d\n", mc[ i ] );
-	}
-	
-	sleep(10);
-	
-	shmdt( &mc );	
-	shmctl( shmid, IPC_RMID, NULL );	
-	return EXIT_SUCCESS;
+    //Vincular la MC a una variable local
+    int *mc = (int*)shmat(shmid, NULL, 0);
+
+    for( int i=0; i< TAM; i++ )
+    {
+        mc[i] = i+1;
+        printf("Proceso A escribió %d\n", mc[i]);
+    }
+
+    //Para frenar hasta que se ejecute B por consola
+    sleep(10);
+
+    //Desvincular la MC del proceso
+    shmdt(&mc);	
+    
+    //Marcar la MC para ser borrada posteriormente 
+    shmctl(shmid, IPC_RMID, NULL);	
+    return EXIT_SUCCESS;
 }
-
-
-
-
-
-
