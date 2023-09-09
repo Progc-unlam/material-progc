75106611890d8f0702ab01796205b293a1f84adc
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 14:53:33 -0300

    Update PipeSerializar.py

--- Ejemplos/Comunicacion/Python/PipeSerializar.py
+++ Ejemplos/Comunicacion/Python/PipeSerializar.py
@@ -3,19 +3,17 @@ import os
 import pickle
 import sys
 
-recepcion, envio = Pipe( False )
+recepcion, envio = Pipe(False)
 
 pid = os.fork()
 if pid < 0:
-	print("Error al crear el nuevo proceso")
-	sys.exit(1)
-		
-if pid:
-	envio.send_bytes( pickle.dumps( (1, 'Yerba "La Tranquera"') ) )
-	envio.close()
-	os.wait() 
-else:
-	print( "Recibido: ", pickle.loads( recepcion.recv_bytes() ) )
-	recepcion.close()	
-	os._exit(0)
+    sys.exit("Error al crear el nuevo proceso")
 
+if pid:
+    envio.send_bytes(pickle.dumps((1, 'Yerba "La Tranquera"')))
+    envio.close()
+    os.wait() 
+else:
+    print("Recibido: ", pickle.loads(recepcion.recv_bytes()))
+    recepcion.close()	
+    os._exit(0)
