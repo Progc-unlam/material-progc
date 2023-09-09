d6e2da835cd116ec045249468bf0f3c3fe5fdfb8
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 15:28:19 -0300

    Update Servidor.java

--- Ejemplos/Comunicacion/Java/Servidor.java
+++ Ejemplos/Comunicacion/Java/Servidor.java
@@ -8,31 +8,28 @@ import java.io.BufferedReader;
 
 class Servidor 
 {
-	private static final int MAX_PEND = 10;
-	private static final int PUERTO = 5000;
-	
-	public static void main(String[] args) throws IOException
-	{
-		ServerSocket ss = new ServerSocket
-		( 
-			PUERTO, 
-			MAX_PEND, 
-			InetAddress.getByName("127.0.0.1") 
-		);
-				
-		System.out.println("Esperando");
-		
-		Socket cs = ss.accept();
-				
-		BufferedReader del_cliente = new BufferedReader
-		(
-			new InputStreamReader(cs.getInputStream())
-		);
+    private static final int MAX_PEND = 10;
+    private static final int PUERTO = 5000;
 
-		System.out.println( del_cliente.readLine() );
-		
-		ss.close();
-		cs.close();	
-	}
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket ss = new ServerSocket
+        ( 
+            PUERTO, MAX_PEND, InetAddress.getByName("127.0.0.1") 
+        );
+
+        System.out.println("Esperando");
+
+        Socket cs = ss.accept();
+				
+        BufferedReader del_cliente = new BufferedReader
+        (
+            new InputStreamReader(cs.getInputStream())
+        );
+
+        System.out.println( del_cliente.readLine() );
+
+        ss.close();
+        cs.close();	
+    }
 }
-
