a7c54c62a7f78cfa7519b22eb8e81407d33863c5
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 12:17:20 -0300

    Update fifoB.c

--- Ejemplos/Comunicacion/C/fifoB.c
+++ Ejemplos/Comunicacion/C/fifoB.c
@@ -5,26 +5,25 @@
 
 typedef struct
 {
-	int dni;
-	char nombre[30];	
+    int dni;
+    char nombre[30];	
 } t_dato;
 
 int main (int argc, char *argv[])
 {
-	t_dato alumno;
-	
-	int r = open("./fifo", O_RDONLY);
-	
-	if( read( r, &alumno, sizeof( t_dato ) ) == -1 )
-	{
-		perror("Error en read()");
-		return EXIT_FAILURE;
-	}	
-	
-	printf("%d\n",alumno.dni);	
-	printf("%s\n",alumno.nombre);	
-	
-	close(r);	
-	return EXIT_SUCCESS;
-}
+    t_dato alumno;
 
+    int r = open("./fifo", O_RDONLY);
+
+    if( read( r, &alumno, sizeof( t_dato ) ) == -1 )
+    {
+        perror("Error en read()");
+        return EXIT_FAILURE;
+    }
+
+    printf("%d\n",alumno.dni);	
+    printf("%s\n",alumno.nombre);	
+
+    close(r);	
+    return EXIT_SUCCESS;
+}
