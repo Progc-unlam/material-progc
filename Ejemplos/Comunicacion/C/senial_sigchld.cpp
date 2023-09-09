f33b47b6ec16f731273f5b52ae712d4daeb02c13
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 14:02:48 -0300

    Update senial_sigchld.cpp

--- Ejemplos/Comunicacion/C/senial_sigchld.cpp
+++ Ejemplos/Comunicacion/C/senial_sigchld.cpp
@@ -3,33 +3,34 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <stdbool.h>
 
-bool fin=false;
+bool fin = false;
 
-void manejador( int sig )
+void manejador(int sig)
 {
-	printf("Señal nro: %d\n",sig);
-	wait( NULL );
-	fin = true;
+    printf("Señal nro: %d\n",sig);
+    wait(NULL);
+    fin = true;
 };
 
 int main(int argc, char *argv[])
 {
-	struct sigaction psa;			
+    struct sigaction psa;			
     psa.sa_handler = manejador;		
     sigaction(SIGCHLD, &psa, NULL); 
-	
-	if( ! fork() )	//Proceso Hijo
-	{
-		return EXIT_SUCCESS;
-	}	
 
-	//Proceso Padre
-	while( ! fin )
-	{
-		usleep(100000);
-	}
-		
-	return EXIT_SUCCESS;
+    if( ! fork() )	
+    {
+        //Proceso Hijo
+        return EXIT_SUCCESS;
+    }
+
+    //Proceso Padre
+    while( ! fin )
+    {
+        usleep(100000);
+    }
+
+    return EXIT_SUCCESS;
 }
-
