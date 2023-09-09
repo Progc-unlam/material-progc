dc59a5a7f4e9e7eb115bb257c38debac3521e6be
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 15:04:36 -0300

    Update senial.py

--- Ejemplos/Comunicacion/Python/senial.py
+++ Ejemplos/Comunicacion/Python/senial.py
@@ -1,22 +1,24 @@
-import os, signal, time, sys
+import os
+import signal
+import time
+import sys
 
 def manejador(sig, frame):
-	print("Recibido ", signal.Signals(sig).name )
+    print("Recibido ", signal.Signals(sig).name)
 
 signal.signal(signal.SIGCONT, manejador)
 
 for i in range(2):
-	pid = os.fork()
-	if pid < 0:
-		print("Error al crear el proceso ",i+1)
-		sys.exit(1)
-	
-	if not pid: #Proceso Hijo
-		signal.raise_signal( signal.SIGSTOP )
-		os._exit(0)
+    pid = os.fork()
+    if pid < 0:
+        print("Error al crear el proceso ",i+1)
+        sys.exit(1)
+
+    if not pid: #Proceso Hijo
+        signal.raise_signal(signal.SIGSTOP)
+        os._exit(0)
 
 time.sleep(2)
-os.killpg( os.getpgrp(), signal.SIGCONT )
+os.killpg(os.getpgrp(), signal.SIGCONT)
 os.wait()
 os.wait()
-
