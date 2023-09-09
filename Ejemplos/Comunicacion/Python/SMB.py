432edef363813b28a8404bb7e8607181e23c486f
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 14:59:02 -0300

    Update SMB.py

--- Ejemplos/Comunicacion/Python/SMB.py
+++ Ejemplos/Comunicacion/Python/SMB.py
@@ -3,14 +3,13 @@ from multiprocessing.resource_tracker import unregister
 
 TAM = 5
 
-mc = shared_memory.SharedMemory( name="mi_mem" )
+mc = shared_memory.SharedMemory(name = "mi_mem")
 
 #Para evitar el comportamiento reportado en: 
 #https://bugs.python.org/issue39959#msg368770
-unregister( mc._name, "shared_memory")  
+unregister(mc._name, "shared_memory")  
 
 for i in range(TAM):
-	print( mc.buf[ i ] )
+    print( mc.buf[i] )
 
 mc.close()
-
