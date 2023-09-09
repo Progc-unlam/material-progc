f47269b0ba348032e13fee4e5945a11ae58b19e8
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-09 13:25:42 -0300

    Update pipe.c

--- Ejemplos/Comunicacion/C/pipe.c
+++ Ejemplos/Comunicacion/C/pipe.c
@@ -12,61 +12,62 @@
 
 struct mensaje
 {
-	int id;
-	char detalle[ LARGO ];
+    int id;
+    char detalle[ LARGO ];
 };
- 
+
 int main( int argc, char **argv )
 {
-	pid_t pid;
-	struct mensaje msj;
+    pid_t pid;
+    struct mensaje msj;
 
-	int p[ DESCRIPTOR ];	//DESCRIPTOR vale 2
-	if( pipe( p ) == -1 )
-	{
-		perror("Error en pipe");
-		return EXIT_FAILURE;
-	}
+    int p[DESCRIPTOR];	//DESCRIPTOR vale 2
+    if( pipe( p ) == -1 )
+    {
+        perror("Error en pipe");
+        return EXIT_FAILURE;
+    }
 
-	pid = fork();
-	if( pid == -1 ) 
-	{
-		perror("Error de fork()");
-		return EXIT_FAILURE;
-	}	
+    pid = fork();
+    if( pid == -1 ) 
+    {
+        perror("Error de fork()");
+        return EXIT_FAILURE;
+    }	
 	
-	if( pid == 0 ) //Hijo		
-	{
-		//ESCRITURA vale 1
-		close( p[ ESCRITURA ] ); 
-		
-		if( read( p[ LECTURA ], &msj, sizeof(struct mensaje) ) == -1)
-		{
-			perror("Error en read()");
-			return EXIT_FAILURE;
-		}
-		
-		printf("Id: %d \nDetalle: %s\n", msj.id, msj.detalle );
-		
-		close( p[ LECTURA ] );
-	}
-	else //Padre
-	{
-		//LECTURA vale 0
-		close( p[ LECTURA ] ); 
-		
-		msj.id = 1;
-		strcpy( msj.detalle, "Yerba La Tranquera" );
-		
-		if( write( p[ ESCRITURA ], &msj, sizeof( struct mensaje ) ) == -1)
-		{
-			perror("Error en write()");
-			return EXIT_FAILURE;
-		}	
-		
-		close( p[ ESCRITURA ] );
-			
-		wait( NULL );
-	}
-	return EXIT_SUCCESS;
+    if( pid == 0 ) //Hijo		
+    {
+        //ESCRITURA vale 1
+        close( p[ESCRITURA] ); 
+
+        if( read( p[LECTURA], &msj, sizeof(struct mensaje) ) == -1)
+        {
+            perror("Error en read()");
+            return EXIT_FAILURE;
+        }
+
+        printf("Id: %d \nDetalle: %s\n", msj.id, msj.detalle);
+
+        close( p[LECTURA] );
+    }
+    else //Padre
+    {
+        //LECTURA vale 0
+        close( p[LECTURA] ); 
+
+        msj.id = 1;
+        strcpy( msj.detalle, "Yerba La Tranquera" );
+
+        if( write( p[ESCRITURA], &msj, sizeof( struct mensaje ) ) == -1)
+        {
+            perror("Error en write()");
+            return EXIT_FAILURE;
+        }	
+
+        close( p[ESCRITURA] );
+
+        wait( NULL );
+    }
+
+    return EXIT_SUCCESS;
 }
