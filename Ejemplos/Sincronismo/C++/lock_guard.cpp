3384ad8c35273217559326e77f743724bd03cdb7
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-16 11:43:40 -0300

    Update lock_guard.cpp

--- Ejemplos/Sincronismo/C++/lock_guard.cpp
+++ Ejemplos/Sincronismo/C++/lock_guard.cpp
@@ -4,32 +4,32 @@
 #include <iostream>
 
 std::mutex mtx;
-int var = 0;
+int value = 0;
 
-void hiloHace()
+void ThreadDo()
 {
-	std::lock_guard<std::mutex> mi_lock(mtx);	//P( mtx ) 
-	var++;	//Región crítica	
-} 			//V( mtx ) - Cuando se destruye mi_lock
+    std::lock_guard<std::mutex> mi_lock(mtx); //P( mtx )
+    value++; //Región crítica
+    //V( mtx ) - Cuando se destruye mi_lock
+}
 
 int main( int argc, char *argv[] )
 {
-	const int MAX_THREADS = 10000;
-	
-	std::thread hilos[ MAX_THREADS ];
-  
-	for( int i=0; i<MAX_THREADS; ++i )
-	{
-    	hilos[ i ] = std::thread( hiloHace );
-	}
-	
-  	for( int i=0; i<MAX_THREADS; ++i ) 
-	{
-		hilos[ i ].join();
-	}	
+    const int kMaxThreads = 10000;
 
-	std::cout<<"var: "<<var<<std::endl;
-	
-	return EXIT_SUCCESS;
+    std::thread hilos[kMaxThreads];
+
+    for (int i=0; i<kMaxThreads; i++)
+    {
+        hilos[i] = std::thread(ThreadDo);
+    }
+
+    for (int i=0; i<kMaxThreads; i++) 
+    {
+        hilos[i].join();
+    }
+
+    std::cout<<"Value: "<<value<<std::endl;
+
+    return EXIT_SUCCESS;
 }
-
