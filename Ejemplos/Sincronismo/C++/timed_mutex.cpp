6d9d00071344440658bce03944f95f4a318eacb1
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-16 12:21:39 -0300

    Update timed_mutex.cpp

--- Ejemplos/Sincronismo/C++/timed_mutex.cpp
+++ Ejemplos/Sincronismo/C++/timed_mutex.cpp
@@ -4,39 +4,37 @@
 #include <iostream>
 #include <chrono>
 
-const int WAIT_TIME = 1000;
-const int SLEP_TIME = 500;
+const int kWaitTime = 1000;
+const int kSleepTime = 500;
 
-std::timed_mutex t_mtx;
+std::timed_mutex timed_mtx;
 
-void hiloHace()
+void ThreadDo()
 {
-	while( ! t_mtx.try_lock_for( std::chrono::milliseconds( WAIT_TIME ) ) )
-	{
-		std::cout<<"...";
-	}
-	std::cout<<" Acceso"<<std::endl;
-	std::this_thread::sleep_for( std::chrono::milliseconds( SLEP_TIME ) );
-	t_mtx.unlock();
-	
+    while (! timed_mtx.try_lock_for(std::chrono::milliseconds(kWaitTime)))
+    {
+        std::cout<<".";
+    }
+    std::cout<<" Acceso"<<std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(kSleepTime));
+    timed_mtx.unlock();
 }
 
 int main( int argc, char *argv[] )
 {
-	const int MAX_THREADS = 10;
-	
-	std::thread hilos[ MAX_THREADS ];
-  
-	for( int i=0; i<MAX_THREADS; ++i )
-	{
-    	hilos[ i ] = std::thread( hiloHace );
-	}
-	
-  	for( int i=0; i<MAX_THREADS; ++i ) 
-	{
-		hilos[ i ].join();
-	}	
-	
-	return EXIT_SUCCESS;
-}
+    const int kMaxThreads = 10;
 
+    std::thread threads[kMaxThreads];
+  
+    for (int i=0; i<kMaxThreads; ++i)
+    {
+        threads[i] = std::thread(ThreadDo);
+    }
+
+    for (int i=0; i<kMaxThreads; ++i)
+    {
+        threads[i].join();
+    }
+
+    return EXIT_SUCCESS;
+}
