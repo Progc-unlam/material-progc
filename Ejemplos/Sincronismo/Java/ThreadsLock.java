bcc538054005ecc79b928004452431872b5fcf31
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-16 17:08:17 -0300

    Update ThreadsLock.java

--- Ejemplos/Sincronismo/Java/ThreadsLock.java
+++ Ejemplos/Sincronismo/Java/ThreadsLock.java
@@ -2,30 +2,30 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadsLock
 {
-	static ReentrantLock mtx = new ReentrantLock();
-	static int value = 0;
-	
-	static class MyThread extends Thread
-	{
-		public void run()
-		{
-			mtx.lock();
-		    value+=1;
-			mtx.unlock();
-        }	      
-	}
-	
-	public static void main(String[] args) throws InterruptedException 
-	{
-		MyThread threadA = new MyThread();
-		MyThread threadB = new MyThread();
-	
-		threadA.start();
-		threadB.start();			
-	
-		threadA.join();
-		threadB.join();
-						
-		System.out.println("Valor: " + value);		
-	}
+    static ReentrantLock mtx = new ReentrantLock();
+    static int value = 0;
+
+    static class MyThread extends Thread
+    {
+        public void run()
+        {
+            mtx.lock();
+            value+=1;
+            mtx.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException 
+    {
+        MyThread threadA = new MyThread();
+        MyThread threadB = new MyThread();
+
+        threadA.start();
+        threadB.start();			
+
+        threadA.join();
+        threadB.join();
+
+        System.out.println("Valor: " + value);		
+     }
 }
