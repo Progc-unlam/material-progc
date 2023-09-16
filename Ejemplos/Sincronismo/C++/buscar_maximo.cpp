397edb32cb5b07a4c9db19d925ee78500e6a42ec
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-16 11:33:05 -0300

    Update buscar_maximo.cpp

--- Ejemplos/Sincronismo/C++/buscar_maximo.cpp
+++ Ejemplos/Sincronismo/C++/buscar_maximo.cpp
@@ -3,47 +3,46 @@
 #include <mutex>
 #include <iostream>
 
-#define MAX_MED 10
-#define MAX_HIL  2
-#define FIN_MED  5
+const int kMaxValues = 10;
+const int kMaxThreads = 2;
+const int kLimit = 5;
 
 std::mutex mtx;
-std::vector<int> mediciones( MAX_MED, 0 ); 
+std::vector<int> values(kMaxValues, 0); 
 
 int max = 0;
 
-void buscarMaximo( int inicio )
+void BuscarMaximo(int begin, int end)
 {
-	for( int i=inicio; i<inicio+FIN_MED; i++)
-	{
-		mtx.lock(); 					//P( mtx )
-		if( mediciones[ i ] > max )		//Región crítica
-			max = mediciones[ i ];
-		mtx.unlock();					//V( mtx )
-	}
+    for (int i=begin; i<end; i++)
+    {
+        mtx.lock();   //P( mtx )
+        if (values[i] > max)
+            max = values[i];
+        mtx.unlock(); //V( mtx )
+    }
 }
 
 int main( int argc, char *argv[] )
 {
-	std::vector<std::thread> hilos;
-	
-	srand( time(NULL) );
-	
-	for( int i=0; i<MAX_MED; i++ )
-		mediciones[ i ]=rand()%100;	
+    std::vector<std::thread> hilos;
 
-	//Creación de los hilos
-	for( int i=0; i<MAX_HIL; i++ ) 
-		hilos.push_back( std::thread( buscarMaximo, i*FIN_MED ) );
+    srand(time(NULL));
 
-	//Espera la finalización de los hilos creados
-	for( int i=0; i<MAX_HIL; i++ ) 
-		hilos[ i ].join();
-	
-	std::cout<<"Medición máxima: "<<max<<std::endl;
+    for (int i=0; i<kMaxValues; i++)
+        values[i]=rand()%100;	
 
-	hilos.clear();
-			
-	return EXIT_SUCCESS;
+    //Creación de los hilos
+    for (int i=0; i<kMaxThreads; i++) 
+        hilos.push_back(std::thread(BuscarMaximo, i*kLimit, i*kLimit+kLimit));
+
+    //Espera la finalización de los hilos creados
+    for (int i=0; i<kMaxThreads; i++) 
+        hilos[i].join();
+
+    std::cout<<"Medición máxima: "<<max<<std::endl;
+
+    hilos.clear();
+
+    return EXIT_SUCCESS;
 }
-
