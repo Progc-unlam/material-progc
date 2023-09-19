6b7cf32eb4f201b9abf1b84c0ae0e433d68cfb85
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2023-09-19 12:18:51 -0300

    Create Atomic.java

---
+++ Ejemplos/Sincronismo/Java/Atomic.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Atomic 
+{
+    static AtomicInteger count = new AtomicInteger(0);
+
+    static class MyThread extends Thread 
+   {
+        public void run()
+        {
+            for (int i=0; i<100; ++i) 
+            {
+                count.getAndIncrement();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException 
+    {
+        MyThread []threads = new MyThread[1000];
+       
+        for(int i=0; i<1000; ++i)
+        {
+            threads[i] = new MyThread();
+        }
+
+        for(int i=0; i<1000; ++i)
+        {
+            threads[i].start();
+        }
+
+        for(int i=0; i<1000; ++i)
+        {
+            threads[i].join();
+        }
+
+        System.out.println("Valor: " + count.get());		
+    }
+}
