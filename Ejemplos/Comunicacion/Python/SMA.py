6bf35a9bf27693d0e69949809a2f697e22459cc4
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2025-05-07 17:41:01 -0300

    Update SMA.py

--- Ejemplos/Comunicacion/Python/SMA.py
+++ Ejemplos/Comunicacion/Python/SMA.py
@@ -1,19 +1,21 @@
 from multiprocessing import shared_memory
 import time
+import numpy 
 
-TAM = 5
+ENTEROS = 5  
+TAM_ENT = numpy.dtype(numpy.int32).itemsize  # Calcular bytes por entero
 
 try:
-    mc = shared_memory.SharedMemory(name="mi_mem", create=True, size=TAM)
+    mc = shared_memory.SharedMemory(name="mi_mem", create=True, size=TAM_ENT*ENTEROS)
     print(f"Memoria compartida creada con nombre: {mc.name}")
 except FileExistsError:
     print("La memoria compartida ya existe.")
-    return shared_memory.SharedMemory(name="mi_mem")
-
-for i in range(TAM):
+   
+for i in range(ENTEROS):
     mc.buf[i] = i+1
 
-time.sleep(15)
+time.sleep(5)
 
 mc.close()
 mc.unlink()
+print("Finalizando SHMA.py")
