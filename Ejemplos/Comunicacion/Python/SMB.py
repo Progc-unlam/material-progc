99985cc249148a53b94a9843dec7af934ce5e235
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2025-05-07 17:45:40 -0300

    Update SMB.py

--- Ejemplos/Comunicacion/Python/SMB.py
+++ Ejemplos/Comunicacion/Python/SMB.py
@@ -1,15 +1,20 @@
 from multiprocessing import shared_memory
 from multiprocessing.resource_tracker import unregister
 
-TAM = 5
+ENTEROS = 5
 
-mc = shared_memory.SharedMemory(name = "mi_mem")
+try:
+    mc = shared_memory.SharedMemory(name="mi_mem")
+    print(f"Memoria compartida accedida: {mc.name}")
+except FileNotFoundError:
+    print("La memoria compartida no existe.")
+    sys.exit(1)
 
-#Para evitar el comportamiento reportado en: 
-#https://bugs.python.org/issue39959#msg368770
-unregister(mc._name, "shared_memory")  
-
-for i in range(TAM):
+for i in range(ENTEROS):
     print( mc.buf[i] )
 
 mc.close()
+#Para evitar el comportamiento reportado en:
+#https://bugs.python.org/issue39959#msg368770
+unregister(mc._name, "shared_memory")
+print("Finalizando SHMB.py")
