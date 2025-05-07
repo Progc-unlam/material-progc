db47b8a6307760469c49effe720fc75bb88793df
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2025-05-07 16:05:59 -0300

    Update FifoA.py

--- Ejemplos/Comunicacion/Python/FifoA.py
+++ Ejemplos/Comunicacion/Python/FifoA.py
@@ -2,16 +2,16 @@ import sys
 import os
 
 try:
-    os.mkfifo( "./fifo", 0o600 )
-
+    os.mkfifo("./fifo", 0o600)  # Crear el FIFO si no existe
 except OSError as err:
-    print(f"Error al crear fifo {err}")
+    print(f"Error al crear fifo: {err}")
     sys.exit(1)
-	
-fifo = os.open( "./fifo", os.O_WRONLY )
 
-os.write( fifo,  "Café La Morenita".encode() )
-
-os.close( fifo )
-
-os.unlink("./fifo")
+try:
+    fifo = os.open("./fifo", os.O_WRONLY)  # Abrir el FIFO para escribir
+    os.write(fifo, "Café La Morenita".encode())  # Escribir en el FIFO
+    os.close(fifo)  # Cerrar el FIFO
+    os.unlink("./fifo")  # Eliminar el FIFO
+except OSError as err:
+    print(f"Error al trabajar con el FIFO: {err}")
+    sys.exit(1)
