3d9deb1c1bd8a2c2b0a293a6145d0935244e5346
Daro Hife <128728554+profesorKiki@users.noreply.github.com>
2025-05-07 16:02:29 -0300

    Update FifoB.py

--- Ejemplos/Comunicacion/Python/FifoB.py
+++ Ejemplos/Comunicacion/Python/FifoB.py
@@ -2,8 +2,18 @@ import os
 
 BYTES = 1024
 
-fifo = os.open( "./fifo", os.O_RDONLY )
+# Intentar abrir el FIFO para lectura
+try:
+    fifo = os.open("./fifo", os.O_RDONLY)
+    
+    # Leer datos del FIFO (hasta BYTES bytes)
+    data = os.read(fifo, BYTES)
+    
+    # Imprimir los datos leídos después de decodificar
+    print(data.decode())  
+    
+    # Cerrar el FIFO después de leer
+    os.close(fifo)
 
-print( os.read( fifo, BYTES ).decode() )
-
-os.close( fifo )
+except OSError as e:
+    print(f"Error al abrir o leer el FIFO: {e}")
